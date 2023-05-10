9b4ab2694363dca5fcba82c6f957581a1a6d16dc
Supanut1911 <ynotmela@gmail.com>
2023-05-10 23:52:57 +0700

    [fiber] - Get params

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,8 +10,35 @@ func main() {
 	app := fiber.New()
 	
 	app.Get("/hello",func(c *fiber.Ctx) error {
-		return nil
+		return c.SendString("GET: hello world")
 	}) 
+
+	app.Post("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("POST: hello world")
+	})
+
+	//parameter
+	// app.Get("/hello/:name", func(c *fiber.Ctx) error {
+	// 	name := c.Params("name")
+	// 	return c.SendString("Hi:" + name)
+	// })
+
+	//parameter optional
+	app.Get("hello/:name/:surname", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		surname := c.Params("surname")
+		return c.SendString("Yo" + name + " " + surname)
+	})
+
+	//paramsInt
+	app.Get("/hello/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		return c.SendString(fmt.Sprintf("Id = %v", id))
+	})
+
 
 	app.Listen(":8888")
 }
